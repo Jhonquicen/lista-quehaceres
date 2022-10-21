f5bcc1e368dc7a41a67b964c7108ea8213c95494
ferney quiceno <104113942+Jhonquicen@users.noreply.github.com>
2022-10-21 09:47:13 -0500

    lista-quehaceres

---
+++ src/Aparte/Aparte.js
@@ -0,0 +1,76 @@
+import React, {  useState} from "react";
+
+
+const Aparte = () => {
+
+        const textStyle = {
+        textDecoration: 'line-through',
+    };
+    
+
+    const[lista, setLista] = useState([])
+    const[input, setInput] = useState([])
+    const[checkedBoxes, setCheckedBoxes] = useState([]);
+    
+
+    const agregarActividad = (actividad) => {
+        const nuevaActividad = {
+            id: Math.random(),
+            actividad: actividad
+        }
+    
+
+    setLista([...lista, nuevaActividad])
+    setInput("");
+    }
+
+
+    const checkeds = (index, value) => {
+        let copy = checkedBoxes;
+        if (value) {
+            copy.push(index);
+        }
+        else {
+            copy = copy.filter(item => item !== index);
+        }
+        setCheckedBoxes([...copy]);
+        console.log(copy);
+    }
+
+    const deleteactividad = (id) => {
+        const nuevaLista = lista.filter((actividad) => actividad.id !== id);
+        setLista(nuevaLista);
+    }
+
+
+    
+
+    return (
+        <div>
+            <ul>
+                <h1>Toda la lista</h1>
+                <input type="text" value={input} onChange={(e) => setInput(e.target.value)}></input>
+                <button onClick={() => agregarActividad(input)}>Add</button> 
+            </ul>
+            <ul>
+                {lista.map((actividad) => (
+                    <li key={actividad.id}>
+                        
+                        <label style={
+                            checkedBoxes.includes(actividad.id)?
+                            textStyle:null
+                            }>{actividad.actividad}</label>
+                        
+                        <input type="checkbox" onChange={(e) => checkeds(actividad.id, e.target.checked)} className="mt-2" /> 
+                        <button onClick={() => deleteactividad(actividad.id)}>delete</button>
+                    </li>
+                ))
+
+                }
+            </ul>
+        </div>
+    )
+}
+
+
+export default Aparte;
---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Aparte from "./Aparte/Aparte";
+
+
+
+class App extends React.Component {
+
+
+  render() {
+
+    return(
+      <div>
+        
+        <Aparte /> 
+        
+      </div>
+    );
+  }
+}
+
+export default App;
